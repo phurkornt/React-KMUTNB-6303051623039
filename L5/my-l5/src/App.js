b465c3408a34656ac7f51a7c561190d09cf0e53c
phurkorn <mysteriousphu@gmail.com>
2023-08-11 15:39:57 +0700

    1.0.5

--- L5/my-l5/src/App.js
+++ L5/my-l5/src/App.js
@@ -3,12 +3,15 @@ import {Header,Content,Footer} from './func-components'
 import Calendar from "./class-components";
 
 
+import Button from '@mui/material/Button';
+
 export default function App(){
     return(
     <>
         <Header/>
         <p><center><Calendar/></center></p>
         <Content/>
+        <Button variant="contained">Hello World</Button>
         <Footer/>
     </>)
 }
--- kmutnb-app/src/class-components.js
+++ kmutnb-app/src/class-components.js
@@ -2,7 +2,6 @@ import React from "react";
 
 
 export default class Calendar extends React.Component{
-    state = {n:Math.floor(Math.random() * 100)}
     getDate(){
         const dayNames = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์','เสาร์']
         const monthNames = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
